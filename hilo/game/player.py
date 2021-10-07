01fd655b67a100996beed449816f8721ef7ad6a3
Shellstriker <91568697+Shellstriker@users.noreply.github.com>
2021-10-07 14:53:00 -0600

    player.py base

---
+++ hilo/game/player.py
@@ -0,0 +1,16 @@
+from director
+class player:
+
+    def __init__(self):
+        self.score = score
+   
+    def can_guess():
+        return  (self.score > 0)
+    
+    def point_change():
+        if guess == correct:
+            score += 100
+        else:
+            score -= 75
+    
+    
