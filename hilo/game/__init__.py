f1341ca01a097354f870112fee6a6d99a8793d51
TannerSiddoway <tanmansidd@gmail.com>
2021-10-07 14:39:10 -0600

    class descriptions
    added what I think classes should do

--- hilo/game/__init__.py
+++ hilo/game/__init__.py
@@ -2,5 +2,9 @@
 The game package contains the classes for playing Hilo.
 """
 class dealer:
+#init, start game
+#dealer should deal cards, update points, display/go again
 
 class player:
+
+#hilo, get points, 
