3fef7e616cb7620aac72c86892f317cb66fecad5
Shellstriker <91568697+Shellstriker@users.noreply.github.com>
2021-10-07 15:02:28 -0600

    Update player.py

--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,16 +1,23 @@
-from director
-class player:
+
+class player(score):
 
     def __init__(self):
         self.score = score
-   
+
     def can_guess():
         return  (self.score > 0)
     
+    def guess(card_1, card_2):
+        if (card_1 > card_2) and guess.lower() == "lower":
+            guess = 'correct'
+        elif (card_1 > card_2) and guess.lower() == "higher":
+            guess = 'incorrect'
+        elif (card_1 < card_2) and guess.lower() == "lower":
+            guess = 'correct'
+
     def point_change():
-        if guess == correct:
+        if guess == 'correct':
             score += 100
         else:
             score -= 75
-    
     
