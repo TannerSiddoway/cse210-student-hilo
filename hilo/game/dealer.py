34c83feba477baa493517c8ab243e9cd9e5cb6bd
Shellstriker <91568697+Shellstriker@users.noreply.github.com>
2021-10-07 18:26:35 -0600

    Player and dealer

--- hilo/game/dealer.py
+++ hilo/game/dealer.py
@@ -1,3 +1,53 @@
+from game.player import player
 class dealer:
-    def __init__(self) -> None:
-        pass
+    def __init__(self):
+        self.keep_playing = True
+        self.score = 300
+        self.player = player()
+    def start_game(self):
+        """Starts the game loop to control the sequence of play.
+        
+        Args:
+            self (Director): an instance of Director.
+        """
+        while self.keep_playing:
+            self.get_inputs()
+            self.do_updates()
+            self.do_outputs()
+
+    
+    def get_inputs(self):
+        """Gets the inputs at the beginning of each round of play. In this case,
+        that means throwing the dice.
+
+        Args:
+            self (Director): An instance of Director.
+            thrower(Thorwer): An instance of Thrower
+        """
+        self.player.guess()
+        
+    def do_updates(self):
+        """Updates the important game information for each round of play. In 
+        this case, that means updating the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        points = self.player.point_change()
+        self.score += points
+        
+    def do_outputs(self):
+        """Outputs the important game information for each round of play. In 
+        this case, that means the dice that were rolled and the score.
+
+        Args:
+            self (Director): An instance of Director.
+        """
+        guess = self.player.guess(card_1, card_2)
+        print(f"You were {guess}")
+        print(f"Your score is: {self.score}")
+        if self.thrower.can_throw():
+            choice = input("guess again? [y/n] ")
+            self.keep_playing = (choice == "y")
+        else:
+            self.keep_playing = False
--- hilo/game/player.py
+++ hilo/game/player.py
@@ -1,26 +1,26 @@
 
-class player(score):
+class player:
 
     def __init__(self):
-        self.score = score
+        self.guess = ""
 
     def can_guess():
         return  (self.score > 0)
     
-    def guess(card_1, card_2):
-        guess = input("Higher/Lower")
-        if (card_1 > card_2) and guess.lower() == "lower":
-            guess = 'correct'
-        elif (card_1 > card_2) and guess.lower() == "higher":
-            guess = 'incorrect'
-        elif (card_1 < card_2) and guess.lower() == "lower":
-            guess = 'correct'
-        elif(card_1 > card_2) and guess.lower() == "higher":
-            guess = 'incorrect'
-        return guess
-    def point_change(guess, score):
-        if guess == 'correct':
-            score += 100
-        else:
-            score -= 75
-        return score    
+    def guess(self, card_1, card_2):
+        self.guess = input("Will the next card be higher or lower (hi/lo)")
+        if (card_1 > card_2) and self.guess.lower() == "lo":
+            self.guess = 'correct'
+        elif (card_1 > card_2) and self.guess.lower() == "hi":
+           self. guess = 'incorrect'
+        elif (card_1 < card_2) and self.guess.lower() == "lo":
+            self.guess = 'correct'
+        elif(card_1 > card_2) and self.guess.lower() == "hi":
+            self.guess = 'incorrect'
+        return self.guess
+   
+    def point_change(self):
+        if self.guess == 'correct':
+            return 100
+        elif self.guess == 'incorrect':
+            return -75
